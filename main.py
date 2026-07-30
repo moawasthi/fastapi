b1443b72e9479f2b6baa6410bc72def1e712a722
moawasthi <mohit.awasthi04@gmail.com>
2026-07-30 10:06:40 +0530

    added get and post methods

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello API!! Hello World!!"}
+
+@app.get("/posts")
+async def  get_posts():
+    return {"post" : 5}
+
+@app.post("/createposts")
+async def create_posts(payload: dict = Body(...)):
+    return {"new post" : f"title is {payload.title} {payload.content}" }
